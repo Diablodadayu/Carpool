d435005ef66f3fe1a7fd6e9105c8793027afe11b
AjalaMark <ajalamark@yahoo.com>
2024-06-19 15:21:29 -0400

    deleted RideController file

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,5 @@
 import express from "express";
 import Controller from "../controllers/Controller.js";
-import RideController from "../controllers/RideController.js";
 import { authenticateToken } from "../middlewares/Middleware.js";
 
 const router = express.Router();
@@ -9,7 +8,7 @@ router
   .post("/login", Controller.post_login)
   .post("/register", Controller.post_register)
   .get("/home", authenticateToken, Controller.get_home)
-  .get("/ride", authenticateToken, RideController.get_ride)
-  .post("/ride", authenticateToken, RideController.post_ride)
+  .get("/ride", authenticateToken, Controller.get_ride)
+  .post("/ride", authenticateToken, Controller.post_ride);
 
 export default router;
