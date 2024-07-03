59f5e622f3b44db2fe3291aab589c4ef3719b0ae
lkBangar <lalitkkumar2696@gmail.com>
2024-07-03 16:16:58 -0400

    find ride card edit

--- client/src/pages/findride.jsx
+++ client/src/pages/findride.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import "../assets/FindRide.css";
+import "../assets/findride.css";
 import "../assets/Home.css";
 import Navbar from "../components/Navbar";
 import SearchBar from "../components/SearchBar";
@@ -47,27 +47,42 @@ const FindRide = () => {
         <Navbar textColor="text-white" />
         <SearchBar />
       </header>
+
       <main className="main-container">
         <h1>Find a Ride</h1>
         <div className="rides-container">
           {rides.map((ride) => (
             <div className="ride-card" key={ride._id}>
-              <div className="ride-details">
-                <h2>{ride.driver.name}</h2>
-                <p>
-                  Route: {ride.startCity.name} to {ride.endCity.name}
-                </p>
-                <p>Leaving: {new Date(ride.departTime).toLocaleString()}</p>
-                <p>Returning: {new Date(ride.returnTime).toLocaleString()}</p>
-                <p>
-                  Travel Date: {new Date(ride.travelDate).toLocaleDateString()}
-                </p>
-                <p>
-                  Car: {ride.carType} {ride.carModel} ({ride.carYear})
-                </p>
-                <p>License Plate: {ride.licensePlate}</p>
-                <p>Car Color: {ride.carColor}</p>
+              <div className="driver-image-container">
+                <div>driver image</div>
               </div>
+
+              <div className="ride-details-container">
+                <div>
+                  <h2>{ride.driver.name}</h2>
+                  <p className="rout">
+                    {ride.startCity.name} to {ride.endCity.name}
+                  </p>
+                  <div className="leaving-returning-container">
+                    <p className="leaving">Leaving:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{new Date(ride.departTime).toLocaleString()}</p>
+                    <p className="returning">Returning:&nbsp;&nbsp;&nbsp; {new Date(ride.returnTime).toLocaleString()}</p>
+                  </div>
+                  {/* <p>
+                    Travel Date: {new Date(ride.travelDate).toLocaleDateString()}
+                  </p>
+                  <p>
+                    Car: {ride.carType} {ride.carModel} ({ride.carYear})
+                  </p>
+                  <p>License Plate: {ride.licensePlate}</p>
+                  <p>Car Color: {ride.carColor}</p> */
+                  }
+                  <div className="pickup-dropoff-container">
+                    <p className="pick-up">Pickup:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; {ride.startCity.name}</p>
+                    <p className="drop-off">Dropoff:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{ride.endCity.name}</p>
+                  </div>
+                </div>
+              </div>
+
               <div className="ride-price-container">
                 <div className="ride-price">${ride.seatPrice}</div>
                 <div className="seats-left">{ride.seatsNumber} seats left</div>
